451bc15b798e3de1e96f0c43d5378dd53e2a403d
Emre Cakmak <emrecakmak93@hotmail.co.uk>
2025-10-01 14:34:41 +0100

    update training script to preprocess data

--- predict.py
+++ predict.py
@@ -1,12 +1,15 @@
 import joblib
 
 # Load trained model
-model = joblib.load("models/sentiment_model.pkl")
+model = joblib.load("models/sentiment_large_model.pkl")
 
 # Example sentences
 examples = [
     "What a wonderful film!",
-    "This was the worst experience ever."
+    "This was the worst experience ever.",
+    "I really loved the story and acting.",
+    "Boring and disappointing movie.",
+    "It was a masterclass in how to do something horribly wrong!"
 ]
 
 for text in examples:
